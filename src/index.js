b14521553b571a342875c801799f48b08b582c09
Elijah Sawyers <elijahsawyers@gmail.com>
2020-01-17 23:02:02 -0600

    Project structure change

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const App = () => {
+  return (
+    <div className="App">
+    </div>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
